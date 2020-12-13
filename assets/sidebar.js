2b8bc209cf8ecabd34dcd5786af15aff0cfa9666
AranNomante <altug.ceylan.yes@gmail.com>
2020-12-13 21:30:14 +0300

    sidebar

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    $('#mySidebar').fadeIn('slow');
 }
 
 function closeNav() {
+    $('#mySidebar').hide();
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
