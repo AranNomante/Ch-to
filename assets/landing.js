74c297754540434797ba7887af6424076c573e90
AranNomante <altug.ceylan.yes@gmail.com>
2020-12-15 19:20:12 +0300

    landing

--- assets/landing.js
+++ assets/landing.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-    $('.list-group a').first().click();
-})
 $('.list-group a').on('click', function() {
     $('.list-group a').removeClass('active');
-    $(this).addClass('active');
+    $('.list-group a').removeClass('customactive');
+    $(this).addClass('customactive');
 });
